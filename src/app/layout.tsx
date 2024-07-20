266c1cb46d232795f1ecc7751a05049f7a5c92db
yash <yashgouravkar@gmail.com>
2024-07-20 07:49:19 +0530

    feat: shadcn components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import "~/styles/globals.css";
 
 import { type Metadata } from "next";
-
+import { Inter } from "next/font/google";
+import TailwindIndicator from "~/components/tailwind-indicator";
 import { TRPCReactProvider } from "~/trpc/react";
+import { cn } from "../lib/utils";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Instagram",
@@ -14,9 +21,13 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("scroll-smooth antialiased", inter.variable)}>
+      <head />
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          {children}
+          <TailwindIndicator />
+        </TRPCReactProvider>
       </body>
     </html>
   );
