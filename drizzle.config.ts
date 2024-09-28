1974d1013763623ff7d135ce75e2b4cbcdd7a6d1
yash <yashgouravkar@gmail.com>
2024-09-28 12:01:21 +0530

    drizzle schema refactor

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,13 @@ import { type Config } from "drizzle-kit";
 import { env } from "~/env";
 
 export default {
-  schema: "./src/server/db/schema.ts",
+  schema: "./src/server/db/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
   },
   tablesFilter: ["turbo-insta_*"],
+  verbose: true, // Print all statements
+  // Always ask for confirmation
+  strict: true,
 } satisfies Config;
--- src/server/db/schema/posts.ts
+++ src/server/db/schema/posts.ts
@@ -1,16 +1,6 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
 import { sql } from "drizzle-orm";
 import { index, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 import { generateId } from "~/app/lib/utils";
-
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 
 export const posts = pgTable(
   "posts",
