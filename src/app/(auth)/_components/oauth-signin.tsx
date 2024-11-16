24d4d55c144c046a7cd406ddf7b7d2295cc5fe62
yash <yashgouravkar@gmail.com>
2024-11-16 08:02:05 +0530

    more auth

--- src/app/(auth)/_components/oauth-signin.tsx
+++ src/app/(auth)/_components/oauth-signin.tsx
@@ -11,7 +11,7 @@ import { getErrorMessage } from "~/lib/utils";
 
 const oauthProviders = [
   { name: "Google", strategy: "oauth_google", icon: "google" },
-  { name: "Discord", strategy: "oauth_discord", icon: "discord" },
+  { name: "Github", strategy: "oauth_github", icon: "gitHub" },
 ] satisfies {
   name: string;
   icon: keyof typeof Icons;
