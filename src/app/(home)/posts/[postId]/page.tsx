74aa29a1ef729d9a4098ce8c8048201297013e3d
yash <yashgouravkar@gmail.com>
2024-12-22 17:06:17 +0530

    css

--- src/app/(home)/posts/[postId]/page.tsx
+++ src/app/(home)/posts/[postId]/page.tsx
@@ -1,13 +1,11 @@
-import { EnterFullScreenIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { AddComment } from "~/components/forms/add-comment";
 import { AspectRatio } from "~/components/ui/aspect-ratio";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
-import { Button, buttonVariants } from "~/components/ui/button";
 import { PostCarousel } from "~/components/utils/post-carousel";
 import { users } from "~/lib/queries/user";
-import { cn, getInitials } from "~/lib/utils";
+import { getInitials } from "~/lib/utils";
 import { api } from "~/trpc/server";
 import { ActionButtons } from "../../components/action-buttons";
 
@@ -26,21 +24,6 @@ export default async function PostModalPage({ params }: PostModalPageProps) {
 
   return (
     <div className="flex w-full gap-2 px-0 sm:justify-end sm:gap-2">
-      <Button
-        className={cn(
-          buttonVariants({
-            variant: "ghost",
-            size: "icon",
-            className:
-              "absolute right-10 top-4 h-auto w-auto shrink-0 rounded-sm bg-transparent p-0 text-foreground opacity-70 ring-offset-background transition-opacity hover:bg-transparent hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground",
-          }),
-        )}
-        asChild
-      >
-        <Link replace href={`/posts/${params.postId}`}>
-          <EnterFullScreenIcon className="size-4" aria-hidden="true" />
-        </Link>
-      </Button>
       <AspectRatio ratio={6.13 / 3} className="border-r shadow-none">
         <PostCarousel files={post.images} />
       </AspectRatio>
