8555101b738ef4674e3039e86480c37a9afc6ffa
yash <yashgouravkar@gmail.com>
2025-02-16 15:52:07 +0530

    event listeners

--- src/app/(home)/posts/[postId]/layout.tsx
+++ src/app/(home)/posts/[postId]/layout.tsx
@@ -1,9 +1,38 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
 import { Dialog, DialogContent } from "~/components/ui/dialog";
+import { useClickOutside } from "~/hooks/use-click-outside";
 
 export default function ModalLayout({ children }: React.PropsWithChildren) {
+  const router = useRouter();
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        router.back();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [router]);
+
+  useClickOutside({
+    ref: contentRef,
+    handler: () => {
+      router.back();
+    },
+  });
+
   return (
     <Dialog defaultOpen={true}>
-      <DialogContent className="max-w-screen-2xl overflow-hidden p-0">
+      <DialogContent
+        ref={contentRef}
+        className="max-w-screen-2xl overflow-hidden p-0"
+      >
         {children}
       </DialogContent>
     </Dialog>
