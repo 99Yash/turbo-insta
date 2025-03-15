8012100e56c1727f0fca9b2f5f48c48dad61d4ab
yash <yashgouravkar@gmail.com>
2025-03-15 10:13:43 +0530

    prompt

--- src/lib/queries/ai.ts
+++ src/lib/queries/ai.ts
@@ -4,16 +4,16 @@ import { openai_model } from "~/config/product";
 export const generateAltText = async (imagePath: string) => {
   const systemPrompt =
     `You will receive an image. ` +
-    `Please create an alt text for the image. ` +
+    `Create an alt text for the image. ` +
     `Be concise. ` +
-    `Use adjectives only when necessary. ` +
-    `Do not pass 100 characters. ` +
+    `Use adjectives when necessary. ` +
+    `Do not pass 99 characters. ` +
     `Use simple language. `;
 
   const { text } = await generateText({
     model: openai_model,
     system: systemPrompt,
-    maxTokens: 30,
+    maxTokens: 25,
     abortSignal: AbortSignal.timeout(5000),
     messages: [
       {
