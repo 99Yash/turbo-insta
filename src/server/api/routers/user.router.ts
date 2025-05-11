231674701f9a815056dbd204aa82a56525ab62cb
yash <yashgouravkar@gmail.com>
2025-05-11 09:17:19 +0530

    asda

--- src/server/api/routers/user.router.ts
+++ src/server/api/routers/user.router.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
-import { findUserById } from "../services/user.service";
+import { getUserById } from "../services/user.service";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
   getUserById: publicProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ ctx: _, input }) => {
-      const user = await findUserById(input.id);
+      const user = await getUserById(input.id);
 
       return user;
     }),
