5cc36af6c21e38f2646382afc39676ce376b69ec
yash <yashgouravkar@gmail.com>
2025-05-24 18:00:49 +0530

    deliver

--- src/lib/providers/ably-provider.tsx
+++ src/lib/providers/ably-provider.tsx
@@ -4,36 +4,36 @@ import Ably from "ably";
 import { AblyProvider } from "ably/react";
 import { api } from "~/trpc/react";
 
-const ablyClient = new Ably.Realtime({
-  authCallback: (_tokenParams, callback) => {
-    const { data: tokenResponse, error } =
-      api.utils.getSocketAuthToken.useQuery(undefined, {
-        enabled: typeof window !== "undefined",
-      });
-
-    if (error) {
-      return callback(error.message, null);
-    }
-
-    if (!tokenResponse) {
-      return callback("No data", null);
-    }
-
-    callback(null, tokenResponse);
-  },
-  /**
-   * Auto-connect in the browser because we don't have cookies in the next-server
-   * and we don't want ably to fire authCallback without cookies
-   * @see https://github.com/ably/ably-js/issues/1742
-   */
-  autoConnect: typeof window !== "undefined",
-  closeOnUnload: false,
-});
-
 export const AblyContextProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
+  const ablyClient = new Ably.Realtime({
+    authCallback: (_tokenParams, callback) => {
+      const { data: tokenResponse, error } =
+        api.utils.getSocketAuthToken.useQuery(undefined, {
+          enabled: typeof window !== "undefined",
+        });
+
+      if (error) {
+        return callback(error.message, null);
+      }
+
+      if (!tokenResponse) {
+        return callback("No data", null);
+      }
+
+      callback(null, tokenResponse);
+    },
+    /**
+     * Auto-connect in the browser because we don't have cookies in the next-server
+     * and we don't want ably to fire authCallback without cookies
+     * @see https://github.com/ably/ably-js/issues/1742
+     */
+    autoConnect: typeof window !== "undefined",
+    closeOnUnload: false,
+  });
+
   return <AblyProvider client={ablyClient}>{children}</AblyProvider>;
 };
