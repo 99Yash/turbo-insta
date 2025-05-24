4d8e44f2df81a4c71310c24512931d2fc2a37a74
yash <yashgouravkar@gmail.com>
2025-05-24 18:19:20 +0530

    as

--- src/lib/providers/ably-provider.tsx
+++ src/lib/providers/ably-provider.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { useUser } from "@clerk/nextjs";
 import Ably from "ably";
 import { AblyProvider } from "ably/react";
+import * as React from "react";
 import { env } from "~/env";
 
 export function AblyContextProvider({
@@ -9,33 +11,53 @@ export function AblyContextProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const ablyClient = new Ably.Realtime({
-    authUrl: `${env.NEXT_PUBLIC_APP_URL}/api/trpc/utils.getSocketAuthToken`,
-    transports: ["web_socket"],
-    // authCallback: (_tokenParams, callback) => {
-    //   const { data: tokenResponse, error } =
-    //     api.utils.getSocketAuthToken.useQuery(undefined, {
-    //       enabled: typeof window !== "undefined",
-    //     });
+  const [ablyClient, setAblyClient] = React.useState<Ably.Realtime | null>(
+    null,
+  );
+  const { user } = useUser();
 
-    //   if (error) {
-    //     return callback(error.message, null);
-    //   }
+  React.useEffect(() => {
+    if (!user) return;
+    const client = new Ably.Realtime({
+      authUrl: `${env.NEXT_PUBLIC_APP_URL}/api/trpc/utils.getSocketAuthToken`,
+      clientId: user.id,
+      transports: ["web_socket"],
+      // authCallback: (_tokenParams, callback) => {
+      //   const { data: tokenResponse, error } =
+      //     api.utils.getSocketAuthToken.useQuery(undefined, {
+      //       enabled: typeof window !== "undefined",
+      //     });
 
-    //   if (!tokenResponse) {
-    //     return callback("No data", null);
-    //   }
+      //   if (error) {
+      //     return callback(error.message, null);
+      //   }
 
-    //   callback(null, tokenResponse);
-    // },
-    /**
-     * Auto-connect in the browser because we don't have cookies in the next-server
-     * and we don't want ably to fire authCallback without cookies
-     * @see https://github.com/ably/ably-js/issues/1742
-     */
-    autoConnect: typeof window !== "undefined",
-    closeOnUnload: false,
-  });
+      //   if (!tokenResponse) {
+      //     return callback("No data", null);
+      //   }
+
+      //   callback(null, tokenResponse);
+      // },
+      /**
+       * Auto-connect in the browser because we don't have cookies in the next-server
+       * and we don't want ably to fire authCallback without cookies
+       * @see https://github.com/ably/ably-js/issues/1742
+       */
+      autoConnect: typeof window !== "undefined",
+      closeOnUnload: false,
+    });
+
+    client.connection.on("connected", () => {
+      console.log("Connected to Ably");
+      setAblyClient(client);
+    });
+
+    return () => {
+      client.close();
+    };
+  }, []);
+
+  if (!ablyClient) return null;
 
   return <AblyProvider client={ablyClient}>{children}</AblyProvider>;
 }
