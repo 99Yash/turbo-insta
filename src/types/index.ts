bd1cad4d90184d284756acbe04f5b49a33d4185d
yash <yashgouravkar@gmail.com>
2025-05-24 13:32:28 +0530

    delete

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,39 +1,9 @@
 import { type SQL } from "drizzle-orm";
 import { z } from "zod";
-import { type Icons } from "~/components/icons";
-
-export interface NavItem {
-  title: string;
-  href?: string;
-  active?: boolean;
-  disabled?: boolean;
-  external?: boolean;
-  icon?: keyof typeof Icons;
-  label?: string;
-  description?: string;
-}
-
-export interface NavItemWithChildren extends NavItem {
-  items?: NavItemWithChildren[];
-}
-
-export interface FooterItem {
-  title: string;
-  items: {
-    title: string;
-    href: string;
-    external?: boolean;
-  }[];
-}
-
-export type MainNavItem = NavItemWithChildren;
-
-export type SidebarNavItem = NavItemWithChildren;
 
 export interface SearchParams {
   [key: string]: string | string[] | undefined;
 }
-
 export const storedFileSchema = z.object({
   id: z.string(),
   name: z.string(),
