fabc4fad06fc1e43dc209c504badf389a5aadac6
yash <yashgouravkar@gmail.com>
2025-05-25 07:10:36 +0530

    remove

--- src/server/api/routers/utils.router.ts
+++
@@ -1,8 +0,0 @@
-import { getSocketAuthToken } from "../services/utils.service";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
-
-export const utilsRouter = createTRPCRouter({
-  getSocketAuthToken: protectedProcedure.query(async ({ ctx }) => {
-    return getSocketAuthToken({ userId: ctx.userId });
-  }),
-});
