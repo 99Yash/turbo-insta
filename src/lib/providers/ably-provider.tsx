b1c715a2f242155aa49f386e0e1b4d93620a8f34
yash <yashgouravkar@gmail.com>
2025-05-25 06:28:35 +0530

    asdasd

--- src/lib/providers/ably-provider.tsx
+++ src/lib/providers/ably-provider.tsx
@@ -4,7 +4,7 @@ import { useUser } from "@clerk/nextjs";
 import Ably from "ably";
 import { AblyProvider } from "ably/react";
 import * as React from "react";
-import { env } from "~/env";
+import { api } from "~/trpc/react";
 
 export function AblyContextProvider({
   children,
@@ -19,25 +19,24 @@ export function AblyContextProvider({
   React.useEffect(() => {
     if (!user) return;
     const client = new Ably.Realtime({
-      authUrl: `${env.NEXT_PUBLIC_APP_URL}/api/trpc/utils.getSocketAuthToken`,
       clientId: user.id,
       transports: ["web_socket"],
-      // authCallback: (_tokenParams, callback) => {
-      //   const { data: tokenResponse, error } =
-      //     api.utils.getSocketAuthToken.useQuery(undefined, {
-      //       enabled: typeof window !== "undefined",
-      //     });
+      authCallback: (_tokenParams, callback) => {
+        const { data: tokenRequest, error } =
+          api.utils.getSocketAuthToken.useQuery(undefined, {
+            enabled: typeof window !== "undefined",
+          });
 
-      //   if (error) {
-      //     return callback(error.message, null);
-      //   }
+        if (error) {
+          return callback(error.message, null);
+        }
 
-      //   if (!tokenResponse) {
-      //     return callback("No data", null);
-      //   }
+        if (!tokenRequest) {
+          return callback("No data", null);
+        }
 
-      //   callback(null, tokenResponse);
-      // },
+        callback(null, tokenRequest);
+      },
       /**
        * Auto-connect in the browser because we don't have cookies in the next-server
        * and we don't want ably to fire authCallback without cookies
@@ -55,7 +54,7 @@ export function AblyContextProvider({
     return () => {
       client.close();
     };
-  }, []);
+  }, [user]);
 
   if (!ablyClient) return null;
 
--- src/server/api/services/utils.service.ts
+++ src/server/api/services/utils.service.ts
@@ -2,9 +2,9 @@ import { ably } from "~/lib/ably";
 import { type GetUserInput } from "../schema/user.schema";
 
 export async function getSocketAuthToken(input: GetUserInput) {
-  const tokenRes = await ably.auth.createTokenRequest({
+  const tokenReq = await ably.auth.createTokenRequest({
     clientId: input.userId,
   });
 
-  return tokenRes;
+  return tokenReq;
 }
