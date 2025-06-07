7edee645e2499ce8eed4b170f4b50ddd73b1996c
yash <yashgouravkar@gmail.com>
2025-06-07 12:24:31 +0530

    client id

--- src/lib/providers/ably-provider.tsx
+++ src/lib/providers/ably-provider.tsx
@@ -1,28 +1,35 @@
 "use client";
 
-import * as Ably from "ably";
+import { useAuth } from "@clerk/nextjs";
+import Ably from "ably";
 import { AblyProvider } from "ably/react";
 
-const ablyClient = new Ably.Realtime({
-  authCallback: (_data, callback) => {
-    fetch("/api/ably")
-      .then((response) => response.json())
-      .then((data) => callback(null, data as Ably.TokenRequest))
-      .catch((error) => callback(error as Ably.ErrorInfo | string, null));
-  },
-  /**
-   * Auto-connect in the browser because we don't have cookies in the next-server
-   * and we don't want ably to fire authCallback without cookies
-   * @see https://github.com/ably/ably-js/issues/1742
-   */
-  autoConnect: typeof window !== "undefined",
-  closeOnUnload: false,
-});
+const ablyClient = (userId: string) =>
+  new Ably.Realtime({
+    authCallback: (_data, callback) => {
+      fetch("/api/ably")
+        .then((response) => response.json())
+        .then((data) => callback(null, data as Ably.TokenRequest))
+        .catch((error) => callback(error as Ably.ErrorInfo | string, null));
+    },
+    /**
+     * Auto-connect in the browser because we don't have cookies in the next-server
+     * and we don't want ably to fire authCallback without cookies
+     * @see https://github.com/ably/ably-js/issues/1742
+     */
+    autoConnect: typeof window !== "undefined",
+    closeOnUnload: false,
+    clientId: userId,
+  });
 
 export const AblyContextProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  return <AblyProvider client={ablyClient}>{children}</AblyProvider>;
+  const { userId } = useAuth();
+  if (!userId) {
+    return <>{children}</>;
+  }
+  return <AblyProvider client={ablyClient(userId)}>{children}</AblyProvider>;
 };
