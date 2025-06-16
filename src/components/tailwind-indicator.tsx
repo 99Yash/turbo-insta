b6474ba83407ce0d0d72f8637c178dc0cdf0ca72
yash <yashgouravkar@gmail.com>
2025-06-16 07:55:38 +0530

    position

--- src/components/tailwind-indicator.tsx
+++ src/components/tailwind-indicator.tsx
@@ -4,7 +4,7 @@ const TailwindIndicator = () => {
   if (env.NODE_ENV === "production") return null;
 
   return (
-    <div className="fixed bottom-1 left-1 z-50 flex h-6 w-6 items-center justify-center rounded bg-zinc-800 p-3 font-mono text-xs">
+    <div className="fixed right-1 top-1 z-50 flex h-6 w-6 items-center justify-center rounded bg-zinc-800 p-3 font-mono text-xs">
       <div className="block sm:hidden">xs</div>
       <div className="hidden sm:block md:hidden lg:hidden xl:hidden 2xl:hidden">
         sm
