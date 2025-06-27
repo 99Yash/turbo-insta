8311aa419cf160e418503a41d366db604f6f07d9
yash <yashgouravkar@gmail.com>
2025-06-27 10:43:01 +0530

    fff

--- src/lib/providers/index.tsx
+++ src/lib/providers/index.tsx
@@ -1,7 +1,8 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import * as React from "react";
 import { TailwindIndicator } from "~/components/tailwind-indicator";
-import { getCachedUser } from "~/lib/queries/user";
+import { getUserById } from "~/server/api/services/user.service";
 import { ClientProviders } from "./client-providers";
 
 interface ProvidersProps {
@@ -13,7 +14,13 @@ interface ProvidersProps {
  * This is an async server component that fetches user data and provides it to client providers
  */
 export async function Providers({ children }: ProvidersProps) {
-  const user = await getCachedUser();
+  const { userId } = await auth();
+
+  if (!userId) {
+    return <div className="h-full">{children}</div>;
+  }
+
+  const user = await getUserById(userId);
 
   return (
     <ClerkProvider>
