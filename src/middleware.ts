8fc7c87b4a0c527d6f70913235dc77c2f1692ae1
yash <yashgouravkar@gmail.com>
2025-06-27 11:07:44 +0530

    bold fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,10 +6,5 @@ export default clerkMiddleware({
 
 export const config = {
   publicRoutes: ["/api/webhooks(.*)"],
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes except /api/webhooks
-    "/(api(?!\/webhooks)|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
