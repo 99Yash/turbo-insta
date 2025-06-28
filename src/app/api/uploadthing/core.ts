e575e0b3ad6d3eedc05264cb10a7d638c2d875d7
yash <yashgouravkar@gmail.com>
2025-06-28 20:33:50 +0530

    utapi v6 to v7

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,40 +1,52 @@
-import { currentUser } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
+import { getCachedUser } from "~/lib/queries/user";
 
 const f = createUploadthing();
 
 export const repligramFileRouter = {
   postImage: f({ image: { maxFileSize: "16MB", maxFileCount: 3 } })
-    // Set permissions and file types for this FileRoute
     .middleware(async ({ req: _req }) => {
-      // This code runs on your server before upload
-      const user = await currentUser();
-      // If you throw, the user will not be able to upload
+      const user = await getCachedUser();
       if (!user) throw new UploadThingError("Unauthorized");
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
 
-      console.log("file url", file.url);
+      console.log("file url", file.ufsUrl);
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { userId: metadata.userId, file };
+      return {
+        userId: metadata.userId,
+        file: {
+          url: file.ufsUrl,
+          name: file.name,
+          size: file.size,
+          type: file.type,
+        },
+      };
     }),
 
   profileImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
     .middleware(async ({ req: _req }) => {
-      const user = await currentUser();
+      const user = await getCachedUser();
       if (!user) throw new UploadThingError("Unauthorized");
       return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       console.log("Profile image uploaded for userId:", metadata.userId);
-      console.log("file url", file.url);
-      return { userId: metadata.userId, file };
+      console.log("file url", file.ufsUrl);
+      return {
+        userId: metadata.userId,
+        file: {
+          url: file.ufsUrl,
+          name: file.name,
+          size: file.size,
+          type: file.type,
+        },
+      };
     }),
 } satisfies FileRouter;
 
