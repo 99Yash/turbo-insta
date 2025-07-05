1cb81d9a8b2e0d6d65bdfb27f4fd3c2a804b4578
yash <yashgouravkar@gmail.com>
2025-07-05 08:15:47 +0530

    remove

--- src/server/api/services/user.service.ts
+++ src/server/api/services/user.service.ts
@@ -1,7 +1,6 @@
 import { TRPCError, getTRPCErrorFromUnknown } from "@trpc/server";
 import { and, eq, ilike, inArray, or } from "drizzle-orm";
 import { ably } from "~/lib/ably";
-import { LIMITS } from "~/lib/constants";
 import { DISALLOWED_USERNAMES } from "~/lib/utils";
 import { db } from "~/server/db";
 import { follows, users } from "~/server/db/schema/users";
@@ -339,22 +338,6 @@ export async function toggleFollow(
       throw e;
     }
 
-    throw new TRPCError({
-      code: getTRPCErrorFromUnknown(e).code,
-      message: getTRPCErrorFromUnknown(e).message,
-    });
-  }
-}
-
-/**
- * Get users by username or name with enhanced search
- * @param query The search query to match against username or name
- * @returns Array of matching users
- */
-export async function getUsersByUsername(query: string) {
-  try {
-    return await searchUsers(query, 0, LIMITS.GET_USERS_BY_USERNAME);
-  } catch (e) {
     throw new TRPCError({
       code: getTRPCErrorFromUnknown(e).code,
       message: getTRPCErrorFromUnknown(e).message,
